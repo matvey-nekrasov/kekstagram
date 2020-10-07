4ae2079bd5ecfa2c2fe82158887e1a09b4e9a839
Matvey Nekrasov Home <metal59@gmail.com>
2020-10-07 15:09:21 +0700

    выполнено разделение на модули

---
+++ js/form-effect.js
@@ -0,0 +1,102 @@
+// Наложение эффекта на изображение
+'use strict';
+
+(() => {
+  const ClassNames = {
+    EFFECTS_PREVIEW: `effects__preview`
+  };
+
+  // Объект для работы с эффетками
+  const effects = {
+    Properties: {
+      none: {
+        FILTER_NAME: ``
+      },
+      chrome: {
+        FILTER_NAME: `grayscale`,
+        MIN: 0,
+        MAX: 1,
+        POSTFIX: ``
+      },
+      sepia: {
+        FILTER_NAME: `sepia`,
+        MIN: 0,
+        MAX: 1,
+        POSTFIX: ``
+      },
+      marvin: {
+        FILTER_NAME: `invert`,
+        MIN: 0,
+        MAX: 100,
+        POSTFIX: `%`
+      },
+      phobos: {
+        FILTER_NAME: `blur`,
+        MIN: 0,
+        MAX: 3,
+        POSTFIX: `px`
+      },
+      heat: {
+        FILTER_NAME: `brightness`,
+        MIN: 1,
+        MAX: 3,
+        POSTFIX: ``
+      }
+    },
+    currentEffect: `none`,
+    isSliderVisible() {
+      return this.Properties[this.currentEffect].FILTER_NAME !== ``;
+    },
+    getStyleFilter(normalizedValue = 1) {
+      const effectProperty = this.Properties[this.currentEffect];
+      const resultValue = (effectProperty.MAX - effectProperty.MIN) * normalizedValue + effectProperty.MIN;
+      if (!effectProperty.FILTER_NAME) {
+        return ``;
+      }
+      return `${effectProperty.FILTER_NAME}(${resultValue}${effectProperty.POSTFIX})`;
+    }
+  };
+
+  const picturesSection = document.querySelector(`.pictures`);
+  const pictureUploadPreviewImage = picturesSection.querySelector(`.img-upload__preview img`);
+  const pictureEffectFieldset = picturesSection.querySelector(`.effects`);
+  const pictureEffectLevelSlider = picturesSection.querySelector(`.effect-level`);
+  const pictureEffectLevelPin = picturesSection.querySelector(`.effect-level__pin`);
+  const effectsArray = Array.from(picturesSection.querySelectorAll(`.effects__radio`)).map((x) => x.value);
+
+  // Обработчик при смене эффекта
+  const onEffectChange = (evt) => {
+    if (!evt.target || !evt.target.matches(`.effects__radio`)) {
+      return;
+    }
+    const effectName = evt.target.value;
+    effects.currentEffect = effectName;
+    pictureEffectReset();
+    pictureUploadPreviewImage.classList.add(`${ClassNames.EFFECTS_PREVIEW}--${effectName}`);
+    if (effects.isSliderVisible()) {
+      pictureEffectLevelSlider.classList.remove(`hidden`);
+    } else {
+      pictureEffectLevelSlider.classList.add(`hidden`);
+    }
+  };
+
+  pictureEffectFieldset.addEventListener(`change`, onEffectChange);
+
+  // Обработчик при изменении слайдера уровня эффекта
+  const onEffectLevelChange = () => {
+    const normalizedEffectValue = pictureEffectLevelPin.offsetLeft / pictureEffectLevelPin.parentElement.offsetWidth;
+    pictureUploadPreviewImage.style.filter = effects.getStyleFilter(normalizedEffectValue);
+  };
+
+  pictureEffectLevelPin.addEventListener(`mouseup`, onEffectLevelChange);
+
+  const pictureEffectReset = () => {
+    const effectsClassNamesArray = effectsArray.map((x) => `${ClassNames.EFFECTS_PREVIEW}--${x}`);
+    pictureUploadPreviewImage.classList.remove(...effectsClassNamesArray);
+    pictureUploadPreviewImage.style.filter = ``;
+  };
+
+  window.formEffect = {
+    reset: pictureEffectReset
+  };
+})();
--- js/form.js
+++ js/form.js
@@ -1,2 +1,78 @@
 // Модуль, который работает с формой редактирования изображения.
 'use strict';
+
+/**
+ * Загрузка изображения и показ формы редактирования ---------------------------------
+ */
+
+const picturesSection = document.querySelector(`.pictures`);
+const bodyTag = document.querySelector(`body`);
+const uploadImageOverlay = picturesSection.querySelector(`.img-upload__overlay`);
+const uploadFileInput = picturesSection.querySelector(`#upload-file`);
+const uploadFileCloseButton = picturesSection.querySelector(`#upload-cancel`);
+const textEditHashtag = picturesSection.querySelector(`.text__hashtags`);
+const textEditComment = picturesSection.querySelector(`.text__description`);
+
+/**
+ * Обработчик нажатия Esc - закрывает попап с настройками
+ * @param {*} evt событие
+ */
+const onPopupEscPress = (evt) => {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    closePhotoEditWindow();
+  }
+};
+
+// Запрет закрытия окна setup при фокусе на поле для ввода хэштега
+textEditHashtag.addEventListener(`focus`, () => {
+  document.removeEventListener(`keydown`, onPopupEscPress);
+});
+
+// Добавление закрытия окна setup при уходе фокуса с поля для ввода хэштега
+textEditHashtag.addEventListener(`blur`, () => {
+  document.addEventListener(`keydown`, onPopupEscPress);
+});
+
+// Запрет закрытия окна setup при фокусе на поле для ввода комментария
+textEditComment.addEventListener(`focus`, () => {
+  document.removeEventListener(`keydown`, onPopupEscPress);
+});
+
+// Добавление закрытия окна setup при уходе фокуса с поля для ввода комментария
+textEditComment.addEventListener(`blur`, () => {
+  document.addEventListener(`keydown`, onPopupEscPress);
+});
+
+// Открытие окна выбора фотографии
+const openPhotoEditWindow = () => {
+  uploadImageOverlay.classList.remove(`hidden`);
+  bodyTag.classList.add(`modal-open`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+// Закрытие окна выбора фотографии
+const closePhotoEditWindow = () => {
+  uploadImageOverlay.classList.add(`hidden`);
+  bodyTag.classList.remove(`modal-open`);
+  uploadFileInput.value = ``;
+  picturesSection.querySelector(`.effects__radio`).checked = true; // Сброс - установить первый элемент checked
+  window.formScale.reset();
+  window.formEffect.reset();
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+// Обработчик при изменении edit.value
+uploadFileInput.addEventListener(`change`, () => {
+  openPhotoEditWindow();
+});
+
+// Обработчик при нажатии на кнопку close popup
+uploadFileCloseButton.addEventListener(`click`, () => {
+  closePhotoEditWindow();
+});
+
+
+/**
+ * Масштаб ----------------------------------------------------
+ */
